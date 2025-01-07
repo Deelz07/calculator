ecef271e4a1fc4a9761d06599df2aba9917e5360
Deelz <deelaka2004@gmail.com>
2025-01-07 20:27:29 +1300

    Setup repository
    Created HTML, CSS and JS files.
    Commented out the logical requirements for each
    project

---
+++ logic.js
@@ -0,0 +1,13 @@
+// Parts
+
+// 1. Creating visuals for button clicks
+// Going to be just simply changing colours and adding pointer when hovering over it
+
+//2. Making screen change on button click
+// A. Store and update text content from each click
+// B. THen Changing text content inside the screen upon button click.
+
+// 3. Evaluating expression
+//A. Store text content in a logical place (same or different from visual text content)?? Where
+//B. Evaluate it in a logical way.
+
